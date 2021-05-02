af97abf89b54549cedbe3ae8e4b5c5ec7d391171
Daniel Rees <daniel_james_rees@hotmail.com>
2021-05-02 18:56:15 -0400

    Very basic polling of weather

--- components/weather.js
+++ components/weather.js
@@ -1,7 +1,21 @@
 import { Card, CardContent, Typography } from "@material-ui/core";
+import { useState } from "react";
+import pollWeather from '../lib/polling';
+
 
 
 export default function Weather({weatherData}) {
+    //let weatherData = "no weather";
+    const [temp,setWeatherData] = useState('No weather data')
+
+    console.log("show up")
+    pollWeather(async () => {
+        console.log("Fetching weather data");
+        const resp = await fetch("http://localhost:8000/api/weather");
+        weatherData = await resp.json();
+        console.log(weatherData);
+        setWeatherData(Number(weatherData?.main?.temp - 272).toFixed(1))
+    },1000);
     return (
         <Card variant="outlined">
             <CardContent >
@@ -9,7 +23,7 @@ export default function Weather({weatherData}) {
                     Here be Weather
                 </Typography>
                 <Typography>
-                    {weatherData ? Number((weatherData?.main?.temp - 272.0).toFixed(1) ) : 'No weather data'}
+                    {temp}
                 </Typography>
             </CardContent>
         </Card>
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Agenda from '../components/agenda';
 
 export default function Home() {
   const tempWeather = {"main": {"temp": 288.2}}
+  
   return (
     <div className="">
       <Head>
@@ -21,7 +22,7 @@ export default function Home() {
         </Box>
         <Grid container spacing={3}>
           <Grid item sm={6} xs={12}>
-            <Weather weatherData={tempWeather}></Weather>
+            <Weather></Weather>
           </Grid>
           <Grid item sm={6} xs={12}>
             <Agenda></Agenda>
